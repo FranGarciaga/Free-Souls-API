671eabeea8ea81dabce85305a6078c83f5eeb9e1
FranGarciaga <frangg_10@hotmail.com>
2024-12-17 13:08:25 +0100

    Feat: Create Packages

---
+++ src/main/java/com/example/FreeSoulsApi/FreeSoulsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FreeSoulsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeSoulsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeSoulsApiApplication.class, args);
+	}
+
+}
